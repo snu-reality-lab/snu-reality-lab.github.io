516bfcd4689575d6bdb6fb10f109cf4a1a8bc401
choisuyeon <suyeon@stanford.edu>
2026-03-18 03:42:22 +0900

    working on the lab website

---
+++ data/news.js
@@ -0,0 +1,12 @@
+window.HOME_NEWS = [
+  {
+    id: "news-2026-03-homepage",
+    dateLabel: "Mar 2026",
+    title: "Website is coming soon!"
+  },
+  {
+    id: "news-2026-03-homepage",
+    dateLabel: "Mar 2026",
+    title: "We're hiring! Send an email to Suyeon if you're interested in joining us."
+  }
+];
---
+++ data/teaching.js
@@ -0,0 +1,13 @@
+window.HOME_TEACHING = [
+  {
+    year: 2026,
+    courseCode: "4190.762",
+    shortTitle: "Computational Imaging",
+    longTitle: "Topics in Computer Graphics - Computational Imaging",
+    shortMeta: "Spring 2026 · SNU",
+    longMeta: "Spring 2026 · Seoul National University",
+    description: "Inverse problems, computational cameras, differentiable optics, ...",
+    teaserImage: "https://engineering.stanford.edu/sites/default/files/styles/card_1900x950/public/media/image/resize_tfoe_wetzstein_0.jpg.webp?h=e4ba68eb&itok=EkGNOhMk",
+    link: "https://reality.snu.ac.kr/class/computational-imaging"
+  }
+];
